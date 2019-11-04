992795b2a7379346b34cd9726a8da66ed99e1071
Luke <spaansl1@hva.nl>
2019-11-04 11:31:16 +0100

    Initial commit

---
+++ app/src/main/java/hva/nl/advancedrecyclerview/ColorItem.kt
@@ -0,0 +1,8 @@
+package hva.nl.advancedrecyclerview
+
+data class ColorItem(
+    var hex: String,
+    var name: String
+) {
+    fun getImageUrl() = "http://singlecolorimage.com/get/$hex/1080x1080"
+}
---
+++ app/src/main/java/hva/nl/advancedrecyclerview/MainActivityViewModel.kt
@@ -0,0 +1,12 @@
+package hva.nl.advancedrecyclerview
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainActivityViewModel : ViewModel() {
+    private val colorRepository = ColorRepository()
+
+    val colorItems = MutableLiveData<List<ColorItem>>().apply {
+        value = colorRepository.getColorItems()
+    }
+}
